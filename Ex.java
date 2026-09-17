5f0ce83b33538b5b0034e22d1a624ead7945851d
tomer923188 <tomerd2010@ofekschool.org.il>
2026-09-17 15:14:38 +0300

    Implement string manipulation tasks in Ex.java
    Added string manipulation tasks for user input handling.

--- Ex.java
+++ Ex.java
@@ -36,11 +36,72 @@ public class Ex {
 	{
 		System.out.println();
 	}
+	
+	public static void stringTask1(Scanner input)
+	{
+		System.out.print("Enter your string: ");
+		String st = input.next();
+		int length = st.length();
+		int min = length;
+		int max = length;
+		for(int i = 1; i < 6; i++)
+		{
+			System.out.print("Enter your string: ");
+			String str = input.next();
+			System.out.println("The length of your string is: " + str.length());
+			length = str.length();
+			if (length < min)
+				min = length;
+			if (length > max)
+				max = length;
+		}
+		System.out.println("Longest string is: "+max+" shortest string is: "+min);
+	}
+	
+	public static void stringTask2(Scanner input)
+	{
+		System.out.print("Enter your first string: ");
+		String first = input.next();
+		System.out.print("Enter a full number: ");
+		int n = input.nextInt();
+		int counter = 0;
+		for(int i = 0; i < n; i++)
+		{
+			System.out.print("Enter a string: ");
+			String str = input.next();
+			if (str.equals(first))
+			{
+				counter++;
+			}
+		}
+		System.out.println(counter+" strings were equal to first");
+	}
+	
+	public static void stringTask3(Scanner input)
+	{
+		System.out.print("Enter your first string: ");
+		String first = input.next();
+		System.out.print("Enter a full number: ");
+		int n = input.nextInt();
+		for(int i = 0; i < n; i++)
+		{
+			System.out.print("Enter a string: ");
+			String str = input.next();
+			if (str.compareTo(first) > 0)
+				System.out.println("after");
+			if (str.compareTo(first) < 0)
+				System.out.println("before");
+			if (str.compareTo(first) == 0)
+				System.out.println("same");
+		}
+	}
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		basicCalculator(input);
-		enterUrAge(input);
+		//basicCalculator(input);
+		//enterUrAge(input);
+		//stringTask1(input);
+		stringTask2(input);
 		input.close();
 	}
 
